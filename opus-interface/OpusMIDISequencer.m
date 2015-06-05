e4aecbb9303d3baf879db17aeb9acb39ea884173
Sam Dooman <samuel.dooman@gmail.com>
2015-06-05 18:01:28 -0400

    [SD] begun initializing structure for data and audio

---
+++ opus-interface/OpusMIDISequencer.m
@@ -0,0 +1,13 @@
+//
+//  OpusMIDISequencer.m
+//  opus-interface
+//
+//  Created by Samuel Dooman on 6/5/15.
+//  Copyright (c) 2015 Sam Dooman. All rights reserved.
+//
+
+#import "OpusMIDISequencer.h"
+
+@implementation OpusMIDISequencer
+
+@end
---
+++ opus-interface/OpusNote.swift
@@ -0,0 +1,13 @@
+//
+//  OpusNote.swift
+//  opus-interface
+//
+//  Created by Samuel Dooman on 6/5/15.
+//  Copyright (c) 2015 Sam Dooman. All rights reserved.
+//
+
+import UIKit
+
+class OpusNote: NSObject {
+   
+}
