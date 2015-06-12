d0e0314b3af6310395c3d17df61a45c8476a5834
Sam Dooman <samuel.dooman@gmail.com>
2015-06-12 16:31:18 -0400

    [SD] more stenciling

--- opus-interface/AudioManager.swift
+++ opus-interface/AudioManager.swift
@@ -6,6 +6,49 @@
 //  Copyright (c) 2015 Sam Dooman. All rights reserved.
 //
 
-protocol AudioManager {
+import AudioToolbox
+
+class OpusAudioManager {
   
+  var _audioGraph: OpusAUGraph
+  var _midiSequenceEditor: OpusMIDIEditor
+  var _midiPlayer: OpusMIDIPlayer
+  
+  init() {
+    _audioGraph = OpusAUGraph()
+    _midiSequenceEditor = OpusMIDIEditor(auGraph: _audioGraph.getAUGraph())
+    _midiPlayer = OpusMIDIEditor(_midiSequenceEditor)
+    
+  }
+  
+  //MARK: Lifecycle
+  
+  //MARK: Edit MIDI Sequence
+  
+  //MARK: Music Playback
+  
+  //MARK: Access fields
+  
+  func hasMIDIEvent(note: UInt8, duration: Float32,
+    beat: MusicTimeStamp) -> Bool {
+      
+      //TODO:(Sam) Fill In Method Stub
+      
+      return true
+  }
+  
+  func insertMIDIEvent(note: UInt8, duration: Float32,
+    beat: MusicTimeStamp) -> Bool {
+      
+      //TODO:(Sam) Fill In Method Stub
+      return true
+  }
+  
+  func removeMIDIEvent(note: UInt8, duration: Float32,
+    beat: MusicTimeStamp) -> Bool {
+      
+      
+      //TODO:(Sam) Fill In Method Stub
+      return true
+  }
 }
