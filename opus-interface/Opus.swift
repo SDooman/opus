12f5e0b580f1756bdba7ec41e38bea75a3c3a530
Sam Dooman <samuel.dooman@gmail.com>
2015-07-06 14:38:52 -0400

    [SD] adds graphics offset constants, finishes structure

--- opus-interface/Opus.swift
+++ opus-interface/Opus.swift
@@ -11,8 +11,19 @@ import UIKit
 
 struct Opus {
   
-  
-  //MARK: - Graphics Constants
-  
+  //MARK: -
   static let UINOTE_TOUCH_THRESHOLD : CGFloat = CGFloat(40.0)
+  
+  
+  //MARK: - Offsets for drawing notes to staff
+  static let WHOLE_NOTE_OFFSET : CGPoint = CGPoint(x: 0, y: 0)
+  
+  static let HALF_NOTE_OFFSET : CGPoint = CGPoint(x: 0, y: 35)
+  
+  static let QUARTER_NOTE_OFFSET : CGPoint = CGPoint(x: 0, y: 70)
+  
+  static let EIGHTH_NOTE_OFFSET : CGPoint = CGPoint(x: -15, y: 35)
+  
+  static let SIXTEENTH_NOTE_OFFSET : CGPoint = CGPoint(x: -15, y: 35)
+
 }
