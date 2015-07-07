80a9eb21786c2ea3e133597c481291ad8b4fbae9
Nick Demarco <nicholas_demarco@brown.edu>
2015-07-07 12:18:24 -0400

    [ND] created pitchesForScaleOnTrebleClef in Opus struct for getting valid pitches on Treble given a key

--- opus-interface/Opus.swift
+++ opus-interface/Opus.swift
@@ -31,19 +31,50 @@ struct Opus {
   
   static let SIXTEENTH_NOTE_OFFSET : CGPoint = CGPoint(x: -15, y: 35)
   
-  //MARK: - [Technical Debt] - Hard coded Treble Clef CMajor Scale
+  //MARK: - Getting Valid Locations on TrebleClef [TECHNICAL DEBT HARD CODED HERE]
   
-  static let trebleCMajor: [Pitch] = [
-    Chroma.D*4,
-    Chroma.E*4,
-    Chroma.F*4,
-    Chroma.G*4,
-    Chroma.A*4,
-    Chroma.B*4,
-    Chroma.C*5,
-    Chroma.D*5,
-    Chroma.E*5,
-    Chroma.F*5,
-    Chroma.G*5
-  ]  
+  static let trebleClefRange: [Pitch] = [
+    Pitch(chroma: Chroma.D, octave: 4),
+    Pitch(chroma: Chroma.Ds, octave: 4),
+    Pitch(chroma: Chroma.E, octave: 4),
+    Pitch(chroma: Chroma.F, octave: 4),
+    Pitch(chroma: Chroma.Fs, octave: 4),
+    Pitch(chroma: Chroma.G, octave: 4),
+    Pitch(chroma: Chroma.Gs, octave: 4),
+    Pitch(chroma: Chroma.A, octave: 4),
+    Pitch(chroma: Chroma.As, octave: 4),
+    Pitch(chroma: Chroma.B, octave: 4),
+    Pitch(chroma: Chroma.C, octave: 5),
+    Pitch(chroma: Chroma.Cs, octave: 5),
+    Pitch(chroma: Chroma.D, octave: 5),
+    Pitch(chroma: Chroma.Ds, octave: 5),
+    Pitch(chroma: Chroma.E, octave: 5),
+    Pitch(chroma: Chroma.F, octave: 5),
+    Pitch(chroma: Chroma.Fs, octave: 5),
+    Pitch(chroma: Chroma.G, octave: 5)
+  ]
+  
+  static func pitchesForScaleOnTrebleClef(#scale: PitchSet) -> [Pitch]{
+    
+    var myPitches: [Pitch] = []
+    
+    for possiblePitch in trebleClefRange {
+      let possibleChroma = possiblePitch.chroma!
+      let checkPitch = Pitch(chroma: possibleChroma, octave: 1)
+      let checkPitch2 = Pitch(chroma: possibleChroma, octave: 2) // C will be one octave higher in notation.
+      if scale.contains(checkPitch) || scale.contains(checkPitch2){
+        myPitches.append(possiblePitch)
+      }
+    }
+    return myPitches
+  }
+  
+  /* HOW TO CALL THIS METHOD:
+  
+  let major = Scale.Major
+  let cMajor = major(Pitch(chroma: Chroma.C, octave: 1)) // Octave is arbitrary, checking on treble clef only.
+  println(Opus.pitchesForScaleOnTrebleClef(scale: cMajor))
+  */
+  
+ 
 }
